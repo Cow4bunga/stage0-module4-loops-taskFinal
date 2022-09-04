0e3fb555de96ec3cbf93a5a5499bae0c4ac33477
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-04 13:10:34 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int mid = cathetusLength / 2;
+        int counter=1;
 
+        for (int i = 0; i < cathetusLength; i++) {
+            for (int j = cathetusLength - i - 1; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for (int k = 1; k <= counter; k++) {
+                System.out.print("*");
+            }
+
+            counter++;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
